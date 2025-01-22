e5a5624c4076b66ea3da42ef01d4a5da07afe7c5
Davidbaa <davidbarrera.ar@gmail.com>
2025-01-22 13:16:56 -0600

    Capturando el valor de campo de entrada

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
 let amigos = [];
+let nombres = (" ");
+
+//Capturar el valor del campo de entrada: Utilizar document.getElementById o document.querySelector para obtener el texto ingresado por el usuario.
+
+nombres = document.getElementById ("amigo").value;
+console.log(nombres);
+
+
 
