5d038fe47b9f0d4fe70744cb63d3c1c45d7c6996
Davidbaa <davidbarrera.ar@gmail.com>
2025-01-22 16:44:45 -0600

    Desarrolla una función, que permita al usuario ingresar un nombre en el campo de texto y añadirlo a la lista de amigos creada anteriormente.

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
 let amigos = [];
-let nombres = (" ");
 
-//Capturar el valor del campo de entrada: Utilizar document.getElementById o document.querySelector para obtener el texto ingresado por el usuario.
+//Función creada para editar el texto en pantalla.
+function asignarTextoElemento (elemento, texto) {
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
+}
 
-nombres = document.getElementById ("amigo").value;
-console.log(nombres);
-
+function agregarAmigo() {
+     //Captura el valor de campo de entrada.
+    let nombres = document.getElementById("amigo").value; 
+    //Valida la entrada.
+    if (nombres === "") { 
+        alert("Por favor, inserte un nombre.");
+    //Verifica si ya existe el nombre.
+    } else if (amigos.includes(nombres)) {
+        asignarTextoElemento('h2', `"${nombres}" ya existe en la lista, ingrese otro nombre.`);
+        } else {
+        //Agregar el nombre a la lista.
+        amigos.push(nombres);
+        asignarTextoElemento('h2','Digite el nombre de sus amigos:');
+        //Actualiza el array de amigos.
+        alert(`Amigo '${nombres}' añadido a la lista.`);
+        // Limpia el campo de entrada.
+        document.getElementById("amigo").value = "";
+    }
+}
 
 
